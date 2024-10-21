bae587f40d0f56bd0ee5c677d6f63b279875cf2a
marcoa16b <marcoferab@gmail.com>
2024-10-20 22:00:22 -0600

    MVP ready to production repair

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,22 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    integrations: [
-        tailwind({
-            applyBaseStyles: false,
-        }),
-        react(),
-    ],
-    experimental: {
-        contentLayer: true,
-    },
+  output: 'server',
+
+  integrations: [
+      tailwind({
+          applyBaseStyles: false,
+      }),
+      react(),
+  ],
+
+  experimental: {
+      contentLayer: true,
+  },
+
+  adapter: vercel(),
 });
